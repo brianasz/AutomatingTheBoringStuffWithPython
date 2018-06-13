a2b8a451f9eae8c52d2790d46dd59a3d09f48574
bsz <bsz@bsz.thinkpad>
2018-06-12 20:17:41 -0500

    RegexSearch: Progress

--- cap8/regexSearch.py
+++ cap8/regexSearch.py
@@ -7,19 +7,23 @@ import os, re
 print("Enter the regular expression: ")
 regEx = input()
 regex = re.compile(regEx)
+#regex = re.compile(r'(%s)'%regEx)
 
 # Identify the .txt files in the current directory
 txtRegex = re.compile(r'\.txt$')
 for filename in os.listdir(os.getcwd()):
   txtFound = txtRegex.search(filename)
   if txtFound != None:
-    print(filename) 
+    print("File found: " + filename) 
   # Search the user-supplied regular expression in all .txt files found in the directory
     # Tasks: Open the file, get the text into a variable and check the regex against the variable containing the string.
     txtFile = open(filename)
     content = txtFile.read()
     txtFile.close()
     match = regex.search(content)
-    print(match.group())
-
-# Nota, the regex is not working fine yet. Improve this. 
+#    print(match.group())
+    match = regex.findall(content)
+    print(match)
+  else:
+    continue
+# Note, the regex is not working fine yet. Improve this. 
